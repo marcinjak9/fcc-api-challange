9661c1a90c1ad52f170d84811411f4ef378a0f46
marcinjak9 <marcinjak9@gmail.com>
2017-04-14 18:37:50 +0200

    finished

--- server.js
+++ server.js
@@ -1,10 +1,28 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var moment = require('moment');
+
+var app = express();
+
+moment().format();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  res.send('insert your date params');
 })
 
+app.get('/:date', function(req,res) {
+	var parameter = req.params.date;
+	var body = {};
+	
+	if (parameter.match(/[a-z]/i)){
+		body.naturalDate = parameter;
+		var date = new Date(parameter);
+		body.unixDate = date.getTime()/1000;
+	} else {
+		body.unixDate = parseInt(parameter);
+		body.naturalDate = moment.unix(parameter).format('YYYY');
+	}
+	res.send(JSON.stringify(body));
+});
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+  console.log('Example app listening on port 3000!');
 })
